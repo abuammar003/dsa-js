da2a3b07ce8074988b380be63a11ceb72a82f6e9
abuammar003 <ammarshah1252@gmail.com>
2026-05-23 14:22:33 +0530

    Completed Video-5

--- Videos-List/Video_5.js
+++ Videos-List/Video_5.js
@@ -70,7 +70,8 @@ let secondMax = arr4[0];
 for(let i = 0; i < arr4.length; i++) {
 
     if(firstMax < arr4[i]) {
-        secondMax = firstMax;   //Yahan Par sMax me Max Rakhna Hai, Then Fir Aage ki Condition me Smax Find Karna Hai.
+        secondMax = firstMax;   //Yahan Par sMax me Max Rakhna Hai, Then Fir Aage ki Condition me Smax Find 
+                                // Karna Hai.
         firstMax = arr4[i]
     } 
     else if(secondMax < arr4[i]) {       
@@ -97,7 +98,7 @@ let sMax = Math.min(arr5[0], arr5[1]);
 for(let i = 0; i < arr5.length; i++) {
 
     if(fMax < arr5[i]) {
-        sMax = fMax;         //Yahan Par sMax me Max Rakhna Hai, Then Fir Aage ki Condition me Smax Find Karna Hai.
+        sMax = fMax;        //Yahan Par sMax me Max Rakhna Hai, Then Fir Aage ki Condition me Smax Find Karna Hai.
         fMax = arr5[i]
     }      
     else if(sMax < arr5[i] && arr5[i] !== fMax) {      //(&& => Condition)- If So Many Max Val. in Array.
@@ -111,4 +112,87 @@ console.log(`Many Same Max Values=> First Max: ${fMax}, Second Max: ${sMax}`);
 
 
 
-// 6- 
+
+// 6- Reversing an Array.
+
+// I- Reversing an Array with Extra Space.
+let arr6 = [10, 20, 30, 40, 50];
+
+let reverseI = arr6.reverse();
+console.log(reverseI);
+
+            //OR
+// II- Reversing an Array with Extra Space. (Using Loops)   {extra Space means => Extra Array}
+// let arr7 = [10, 20, 30, 40, 50];
+
+// let newArr7 = new Array(arr7.length);         //newArr7 => Extra Array.
+// let j = 0;
+
+// for (let i = arr7.length - 1; i >= 0; i--) {      //let i = arr7.length -1 ;    index of: i = 4 (Last).
+//     newArr7[j] = arr7[i];          //Here index of: j = 0, & i = 4.  (j => Value of newArr7).
+//     j++;
+// }
+// console.log(newArr7);
+
+
+ 
+// III- Reversing an Array without Extra Space.    (without Extra Array).
+// let arr8 = [10, 20, 30, 40, 50]; 
+
+// let i = 0, j = arr8.length -1;       //Two Pointers Algorithms. (i & j=> Pointers)
+
+// while( i != j ) {
+//     let temp = arr8[i];
+//     arr8[i] = arr8[j];
+//     arr8[j] = temp;
+
+// }
+// console.log(arr8);
+
+
+
+
+
+// 7- Move Zeroes to Start & End.
+
+// I- Move zeroes to Start.
+    //Logic is, i & j both starts from 0th index. & we Runs Loop of i, and if the Value of i is 0 then it Swaps to j.
+
+let arr9 = [1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0];
+
+let i = 0, j = 0;
+
+while ( i < arr9.length ) {
+    
+    if( arr9[i] == 0 ) {         //Here If value of Array == 0, then swap it (i) on the place (j);
+
+        let temp = arr9[i];
+        arr9[i] = arr9[j];
+        arr9[j] = temp;
+        j++ 
+    
+    }
+    i++;
+
+}
+console.log(arr9);
+
+
+
+// II- Move Zeroes to End.
+let arr10 = [1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1];
+
+let i2 = 0, j2 = 0;
+
+while ( i2 < arr10.length ) {
+
+    if(arr10[i2] == 1) {
+        let temp = arr10[i2];
+        arr10[i2] = arr10[j2];
+        arr10[j2] = temp;
+        j2++
+    } 
+    i2++
+
+}
+console.log(arr10);
