16aa94f9f8e00140db338a017a3e44ea04486d15
abuammar003 <ammarshah1252@gmail.com>
2026-05-26 15:59:06 +0530

    Completed Type 2nd Problems

--- Videos-List/Video_6.js
+++ Videos-List/Video_6.js
@@ -9,9 +9,9 @@ let arr1 = [1, 2, 3, 4, 5];            // output => arr1 = [2, 3, 4, 5, 1];
 
 let lastElement = arr1[0];
 
-for (let i = 0; i < arr1.length -1; i++) {
+for (let i = 0; i < arr1.length -1; i++) { 
 
-    arr1[i] = arr1[i +1];     //Here it puts (i +1) on (ith) index 
+    arr1[i] = arr1[i +1];     //Here it puts Value of(i +1)th Index on (ith) index 
     
 }
 arr1[arr1.length -1] = lastElement;
@@ -38,19 +38,58 @@ console.log(arr2);
 // 2- Left & Right Rotation By k Element.      (k = User's Defined Value {Means by How much index to Rotate Element})
 
 // I- Left Rotation by k Element.
-let arr3 = [1, 2, 3, 4, 5];        //Output => arr3 = [3, 4, 5, 1, 2];
+// let arr3 = [1, 2, 3, 4, 5];        //Output => arr3 = [3, 4, 5, 1, 2];
 
-let k = 2;           //Means Rotating by 2 elements.
+// let k = 2;           //Means Rotating by 2 elements.
 
-let newArr3 = [];
+// let newArr3 = [];
 
-for (let i = k; i < arr3.length; i++) {
-    newArr3.push(arr3[i])
+// for (let i = k; i < arr3.length; i++) {
+//     newArr3.push(arr3[i])
+// }
+
+// for (let i = 0; i < k; i++ ) {
+//     newArr3.push(arr3[i]);
+// }
+// console.log(newArr3)
+
+
+
+// II- Right Rotation by k Element.
+let arr4 = [1, 2, 3, 4, 5, 6, 7];         //Output => arr4 = [5, 6, 7, 1, 2, 3, 4];
+
+let k2 = 3;
+let newArr4 = [];
+
+for (let i = arr4.length -k2; i < arr4.length; i++) {
+    newArr4.push(arr4[i]);
 }
 
-for (let i = 0; i < k; i++ ) {
-    newArr3.push(arr3[i]);
+for (let i = 0; i < arr4.length -k2; i++) {
+    newArr4.push(arr4[i])
 }
-console.log(newArr3)
+console.log(newArr4);
 
 
+
+// III- Rotation of Element By K value from Propmt. (Left Rotation).
+// let arr6 = [1, 2, 3, 4, 5];
+
+// let k4 = Number(prompt("Enter K Value"));
+
+// for (let j = 0; j < k4; j++) {              //Looping/Rotating the Array's Element By k number of Time.
+   
+//     //Normal Left Rotation By 1 element.
+//     let lastValue = arr6[0];
+//     for (let i = 0; i < arr6.length -1; i++) {
+//         arr6[i] = arr6[i +1];
+//     }
+//     arr6[arr6.length -1] = lastValue;
+
+// }
+// console.log(arr6)
+
+
+
+
+// 3- 
