4a18400818ba8ffc1f337eb0336f68752088f62f
abuammar003 <ammarshah1252@gmail.com>
2026-07-08 20:09:55 +0530

    Solved Q-No. 6-9 of Arrays

--- Videos-List/JS InterView Questions/Array.js
+++ Videos-List/JS InterView Questions/Array.js
@@ -1,3 +1,77 @@
 // Interview Questions on Arrays =>
 
-// 1. 
+
+// 1. Create an array of your top 5 favorite movies and print them to the console.
+let favMovieArray = ["Avengers", "Obession", "3-idiots", "Man of War", "The Punisher"];
+favMovieArray.forEach((movies) => {
+    console.log(movies);
+});
+
+
+
+//2. Find and log the second element of the array.
+let arr = [10, 20, 30, 40, 50];
+console.log("Second Element of the Array:", arr[1]);
+
+
+//3. Add Two (2) new elements to the start of the array.
+let arr2 = [1, 2, 3, 4, 5];
+arr2.unshift(-1, 0);
+console.log(arr2);
+
+
+
+//4. Remove the last element from the array.
+let arr3 = [1, 2, 3, 4, 5];
+arr3.pop();
+console.log(arr3);
+
+
+
+//5. Use .slice() to extract the first five elements of the array.
+let arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("First 5 elements of the Array:", arr4.slice(0, 5));
+        //OR
+let firstFiveElementsOfArr4 = arr4.slice(0,5);
+console.log(firstFiveElementsOfArr4);
+
+
+
+//6. Find the index of a specific element in the array using .indexOf().
+let arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(arr5.indexOf(7));  //output: 6;
+
+
+
+// 7. Check if a value exists in the array using .includes().
+let arr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Chacking 5 is exists:", arr6.includes(5));  //output: true;
+console.log("Chacing 15 is exists:", arr6.includes(15));  //output: false;
+
+
+
+// 8. Combine two arrays into one using .concat().
+let arr7 = [1, 2, 3, 4, 5];
+let arr8 = [6, 7, 8, 9, 10];
+let combineTwoArrays = arr7.concat(arr8);
+console.log(`Combination of Two Arrays is: ${combineTwoArrays}`);
+
+ 
+
+// 9. Sort the array in Ascending Odred.
+let arr9 = [3, 4, 8, 1, 2, 5];
+let ascendingOrderedArray = arr9.sort((a, b) => a - b);
+console.log("Array sotred in Ascending Order using sort():", ascendingOrderedArray);
+                // OR
+for(let i = 0; i <= arr9.length; i++) {
+    for(let j = i +1; j <= arr9.length; j++) {
+
+        if(arr9[j] < arr9[i]) {
+            let tempValue = arr9[i];
+            arr9[i] = arr9[j];
+            arr9[j] = tempValue;
+        }
+
+    }
+}
+console.log("Array sotred in Ascending Order using Loop:", arr9);
--- Videos-List/JS InterView Questions/Loops.js
+++ Videos-List/JS InterView Questions/Loops.js
@@ -1,9 +1,11 @@
 // Interview Questions on Loops =>
 
+
 // 1. Write a for loop that prints the numbers from 1 to 50.
 for (let i = 1; i <= 50; i++ ) {
     console.log(i);
 }
+
 
 
 // 2. Write Use a while that somes to print the numbers from 1 to 10.
@@ -31,6 +33,7 @@ for (let i3 = 1; i3 <= 20; i3++) {
 }
 
 
+
 // 4. Write a for loop that findes the Factorial of a given number.
 let num = 5;
 let factorial = 1;
@@ -46,6 +49,7 @@ console.log(`Factorial of ${num} is: ${factorial}`);
     // console.log(`Factorial of ${num} is: ${fact}`);
 
 
+
 // 5. Use a for loop to Reverse an Array.
 let arr = [1, 2, 3, 4, 5 ];
 let reverseArr = [];
@@ -56,4 +60,28 @@ for(let i=arr.length -1; i >=0; i--) {
 console.log(reverseArr);
 
 
-// 6. 
+
+// 6. write the loop that logs numbers from 1 to 100 divisible by 5.
+let i5 = 1;
+let numsDivisibleBy5 = [];
+while(i5 <= 100) {
+    if(i5 % 5 === 0) {
+        numsDivisibleBy5.push(i5);
+    }
+    i5++;
+}
+console.log('The Numbers Divisible by 5 are:' ,numsDivisibleBy5);
+
+
+
+// 7. Write a for...in loop to iterate over an object and log its keys.
+let obj = {name: 'a', email: 'a@email.com', age: 25};
+
+for(let key in obj) {
+    console.log(key);
+}
+
+//8. Write a for...in loop to iterate over an object and log its keys and values.
+for(let key in obj) {
+    console.log(key, obj[key]);
+}
