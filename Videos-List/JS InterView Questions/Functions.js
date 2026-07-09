24b2e806afc7d87fe174c370dee0274fac5a3b31
abuammar003 <ammarshah1252@gmail.com>
2026-07-09 18:08:06 +0530

    Solved 2-Questions of Functions

--- Videos-List/JS InterView Questions/Functions.js
+++ Videos-List/JS InterView Questions/Functions.js
@@ -13,6 +13,27 @@ function checkEvenOdd() {
 }
 checkEvenOdd();
 
+                    // OR
+function evenOddCheck(value) {
+    if(value %2 === 0) return "Even";
+    else return "Odd";
+}
+console.log("The value is:", evenOddCheck(13));
 
 
-// 2. 
+
+
+// 2. Write a Function that accepts an Array and return the sum of its elements.
+function enterArrayAndSum() {
+    let arr = [1, 2, 3, 4, 5];
+    let sum = 0;
+    for(let i = 0; i <= arr.length -1; i++) {
+        sum += arr[i];
+    }
+    console.log("The Sum of the Array:", sum);
+}  
+enterArrayAndSum();
+
+
+
+// 3. 
